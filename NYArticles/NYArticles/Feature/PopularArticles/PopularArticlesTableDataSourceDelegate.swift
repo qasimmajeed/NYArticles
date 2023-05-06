0d7a0e881e733610a3dfe1261563cda93fcf546d
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-05-07 01:31:06 +0400

    redo

--- NYArticles/NYArticles/Feature/PopularArticles/PopularArticlesTableDataSourceDelegate.swift
+++ NYArticles/NYArticles/Feature/PopularArticles/PopularArticlesTableDataSourceDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-protocol PopularArticlesTableDataSourceDelegateProtocol: UITableViewDelegate, UITableViewDataSource {
+protocol PopularArticlesTableDataSourceDelegateProtocol: AnyObject, UITableViewDelegate, UITableViewDataSource {
     var viewModel: PopularArticlesViewModel { get set }
 }
 
