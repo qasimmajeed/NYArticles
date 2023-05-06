a2e7d6663ef62845dcafdeaf3c6a2d7e863990e4
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-05-06 16:36:14 +0400

    Folder struct organized Coordinator protocol is create to handle the screen handling in the app

---
+++ NYArticles/NYArticles/Common/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  NYArticles
+//
+//  Created by Muhammad Qasim Majeed on 06/05/2023.
+//
+
+import Foundation
+
+protocol Coordinator {
+    /// This is start the application flow or navigation.
+    func start()
+}
