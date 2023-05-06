731c0c2d5ac0a8325978ade9938a7d916330e5f6
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-05-06 16:39:46 +0400

    AppCoordinator will be use to start the application Main story board is deleted

---
+++ NYArticles/NYArticles/AppSetup/AppCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  AppCoordinator.swift
+//  NYArticles
+//
+//  Created by Muhammad Qasim Majeed on 06/05/2023.
+//
+
+import UIKit
+
+protocol AppCoordinatorProtocol: Coordinator {}
+
+struct AppCoordinator: AppCoordinatorProtocol {
+    // MARK: - Private Properties
+
+    private let window: UIWindow
+    private let navigation: UINavigationController
+
+    // MARK: - Init
+
+    init(window: UIWindow, navigation: UINavigationController) {
+        self.window = window
+        self.navigation = navigation
+    }
+
+    // MARK: - Coordinator
+
+    func start() {
+        window.rootViewController = navigation
+        window.makeKeyAndVisible()
+    }
+}
+
