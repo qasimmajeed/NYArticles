79c4d85c6ec25589fef3b61317f9b125a2e3dc0d
Muhammad Qasim Majeed <qasim_majeed2003@yahoo.com>
2023-05-06 15:14:16 +0400

    main project is create to the workspace

---
+++ NYArticles/NYArticles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYArticles
+//
+//  Created by Muhammad Qasim Majeed on 06/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
